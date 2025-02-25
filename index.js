59be57b31d78d556a69bfd546bbcae49b24a5b1f
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-02-25 11:29:20 -0300

    funcionalidade de criar uma conta

--- index.js
+++ index.js
@@ -20,7 +20,51 @@ function operations() {
     }])
     .then((answer) => {
         const action = answer['action'];
-        console.log(action);
+        
+        if (action === 'Criar conta') {
+            createAccount();
+        }
     })
     .catch((error) => console.log(error))
-}
+};
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher nosso banco!'));
+    console.log(chalk.green('Defina as opções da sua conta a seguir'));
+
+    buildAccount();
+};
+
+function buildAccount() {
+    inquirer.prompt([{
+        name: 'accountName',
+        message: 'Digite um nome para a sua conta:'
+    }])
+    .then((answer) => {
+        const accountName = answer['accountName'];
+
+        console.info(accountName);
+
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts');
+        }
+
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'));
+            buildAccount();
+            return;
+        }
+
+        fs.writeFileSync(
+            `accounts/${accountName}.json`, 
+            JSON.stringify({ "balance": 0 }),
+            function(error) {
+                console.log(chalk.bgRed.black(error));
+            }
+        )
+
+        console.log(chalk.green('Parabéns, sua conta foi criada com sucesso!'));
+        operations();
+    })
+    .catch((error) => console.log(error));
+};
