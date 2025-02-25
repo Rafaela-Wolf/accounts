4cff7ed8bfcb6c7eb4337ee6beddcf5f603744bd
Rafaela Wolf <rafaelawolf52@gmail.com>
2025-02-25 10:58:02 -0300

    inicialização do sistema

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const chalk = require("chalk");
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+operations();
+
+function operations() {
+
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: [
+            'Criar conta',
+            'Consultar saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ]
+    }])
+    .then((answer) => {
+        const action = answer['action'];
+        console.log(action);
+    })
+    .catch((error) => console.log(error))
+}
